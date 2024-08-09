b008905da1ee32997883b901d90a8ab241f33637
Fomin Alexey <fomin.alexey@outlook.com>
2024-08-09 13:46:01 +0300

    post soviet, mrad fix

--- MRAD/MRAD/scripts/Game/Drg_NotepadRtComponent.c
+++ MRAD/MRAD/scripts/Game/Drg_NotepadRtComponent.c
@@ -3,6 +3,49 @@ class Drg_NotepadRtComponentClass : ScriptComponentClass
 {
 }
 
+class Drg_NotepadRtComponent : ScriptComponent
+{
+	[Attribute("")]
+	ResourceName m_sPathLayout;
+	Widget m_wRTRoot;
+	
+	[Attribute("", UIWidgets.EditBoxMultiline)]
+	string m_sText;
+	
+		
+	//------------------------------------------------------------------------------------------------
+	override void EOnInit(IEntity owner)
+	{
+		m_wRTRoot = GetGame().GetWorkspace().CreateWidgets(m_sPathLayout);
+		RTTextureWidget rtTexture = RTTextureWidget.Cast(m_wRTRoot.FindAnyWidget("drg_Main_RTTexture"));
+		rtTexture.SetGUIWidget(owner, 1);
+		
+		RichTextWidget richRextureWidget = RichTextWidget.Cast(m_wRTRoot.FindAnyWidget("drg_Text"));
+		richRextureWidget.SetText(m_sText);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void OnPostInit(IEntity owner)
+	{
+		SetEventMask(owner, EntityEvent.INIT);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void OnDelete(IEntity owner)
+	{
+		if (m_wRTRoot)
+			m_wRTRoot.RemoveFromHierarchy();
+	}	
+}
+
+
+/* Статическая версия (не подходит для генерации в игре)
+
+[ComponentEditorProps(category: "GameScripted/Misc", description: "")]
+class Drg_NotepadRtComponentClass : ScriptComponentClass
+{
+}
+
 
 class Drg_NotepadRtComponent : ScriptComponent
 {
@@ -47,3 +90,4 @@ class Drg_NotepadRtComponent : ScriptComponent
 			m_wRTRoot.RemoveFromHierarchy();
 	}	
 }
+*/
